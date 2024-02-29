ebd1b680d536e51a11bb5d1a476f7830f6c83472
Nat <nathalie_sa@live.fr>
2024-02-29 12:13:38 +0100

    update route & schema user

--- models/users.model.js
+++ models/users.model.js
@@ -1,12 +1,54 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
-firstname: String,
-username: String,
-password: String,
-token: String,
+// const userSchema = mongoose.Schema({
+// firstname: String,
+// username: String,
+// password: String,
+// token: String,
   
-});
+// });
+
+//AJOUTER NVX SCHEMA USER AVEC THEME & THINGS LIKE POUR CORRESPONDRE AVEC MOBILE
+//+ RESTE DU PROFIL: PHOTO, PERSONNE SUIVI, etc?
+
+
+const userSchema = mongoose.Schema(
+    {
+        firstname: {
+            type: String,
+            required: true,
+            trim: true,
+            message: ({ value }) => `Username ${value} is not valid.`,
+        },
+        username: {
+            type: String,
+            unique: true,
+            index: true,
+            required: true,
+            trim: true,
+            message: ({ value }) => `Username ${value} already exist.`,
+            // validate: {
+            //     validator: val => /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/?.test(val),
+            //     message: ({ value }) => `${value} is not a valid email address.`,
+            // },
+        },
+        password: {
+            type:String,
+            select: false,
+            required: true
+        },
+        token: String,
+        type: {
+            type: String,
+            enum: ["admin", "user", "moderator"],
+            default: "user",
+        },
+        dateCreated: Date,
+    }, 
+    { timestamps: true, toJSON: { virtuals: true} }
+     
+);
+
 
 const User = mongoose.model('users', userSchema);
 
--- routes/users.route.js
+++ routes/users.route.js
@@ -9,50 +9,96 @@ const bcrypt = require('bcrypt');
 
 /* ENREGISTREMENT HACKATWEET = SIGN UP*/
 
-router.post('/signup', (req, res) => {
-  if (!checkBody(req.body, ['username', 'password'])) {
-    res.status(500).json({ result: false, error: 'Missing or empty fields' });
-    return;
-  }
-/* CHECK IF USER ALREADY REGISTERED*/
-  User.findOne({ username: req.body.username }).then(data => {
-    if (data === null) {
-      const hash = bcrypt.hashSync(req.body.password, 10);
+// router.post('/signup', (req, res) => {
+//   if (!checkBody(req.body, ['username', 'password'])) {
+//     res.status(500).json({ result: false, error: 'Missing or empty fields' });
+//     return;
+//   }
+// /* CHECK IF USER ALREADY REGISTERED*/
+//   User.findOne({ username: req.body.username }).then(data => {
+//     if (data === null) {
+//       const hash = bcrypt.hashSync(req.body.password, 10);
   
-      const newUser = new User({
-        firstname: req.body.firstname,
-        username: req.body.username,
-        password: hash,
-        token: uid2(32),
-      });
+//       const newUser = new User({
+//         firstname: req.body.firstname,
+//         username: req.body.username,
+//         password: hash,
+//         token: uid2(32),
+//       });
   
-      newUser.save().then(newDoc => {
-        res.json({ result: true, newDoc });
-      });
-    } else {
-      // USER ALREADY EXISTS
-      res.status(500).json({ result: false, error: 'User already exists' });
+//       newUser.save().then(newDoc => {
+//         res.json({ result: true, newDoc });
+//       });
+//     } else {
+//       // USER ALREADY EXISTS
+//       res.status(500).json({ result: false, error: 'User already exists' });
+//     }
+//   });
+//   });
+
+
+
+  router.post('/signup', async(req, res) => {
+    if (!checkBody(req.body, ['username', 'password'])) {
+      res.status(500).json({ result: false, message: 'Merci de remplir tous les champs' });
+      return;
     }
-  });
-  });
+  /* CHECK IF USER ALREADY REGISTERED*/
+    User.findOne({ username: req.body.username }).then(async data => {
+      if (data === null) {
+        const hash = bcrypt.hashSync(req.body.password, 10);
+        const { firstname, username, /*dateCreated*/ } = req.body;
+    
+        const newUser = new User({
+          firstname: firstname,
+          username: username,
+          password: hash,
+          token: uid2(32),
+          dateCreated: new Date(),
+          // dateCreated: dateCreated,
+        });
+
+        try {
+          const userCreate = await newUser.save();
+          console.log(userCreate);
+          newUser.save().then(newDoc => {
+            res.json({ result: true, newDoc });
+          });
+        } catch (error) {
+          if (error instanceof MongoServerError && error.code === 11000) {
+            res.status(400).json({ result: false, message: "Le nom d'utilisateur existe déjà" });
+          } else {
+            console.error(error);
+            res.status(500).json({ result: false, message: "Erreur lors de la création de l'utilisateur" });
+          }
+        }
+      } else {
+        // USER ALREADY EXISTS
+        res.status(500).json({ result: false, message: "Le nom d'utilisateur existe déjà" });
+      }
+    });
+    });
 
 
 
 /* LOGIN HACKATWEET = SIGN IN  */
 
-router.post('/signin', (req, res) => {
+router.post('/signin', async (req, res) => {
   if (!checkBody(req.body, ['username', 'password'])) {
-    res.status(500).json({ result: false, error: 'Missing or empty fields' });
+    res.status(500).json({ result: false, message: 'Merci de remplir tous les champs' });
     return;
   }
-
-  User.findOne({ username: req.body.username }).then(data => {
-    if (data && bcrypt.compareSync(req.body.password, data.password)) {
-      res.json({ result: true, data });
-    } else {
-      res.status(500).json({ result: false, error: 'User not found or wrong password' });
-    }
-  });
+  try {
+    const data = await User.findOne({ username: req.body.username }).select('+password')
+      if (data && bcrypt.compareSync(req.body.password, data.password)) {
+        res.json({ result: true, data });
+      } else {
+        res.status(500).json({ result: false, message: 'Utilisateur non trouvé ou mauvais mot de passe' });
+      }
+    
+  } catch (error) {
+    res.status(500).json({result: false, message: "Erreur serveur"})
+  }
 });
 
 
